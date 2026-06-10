646db4bf05ba2892f4e02d1706dafbc0682e525f
shotgun47 <mesh47@naver.com>
2026-06-10 18:26:23 +0900

    Delete analysis directory

--- analysis/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Vulnerability analysis package."""
-
-from analysis.analyzer import AnalyzerConfig, VulnerabilityAnalyzer, analyze
-
-__all__ = ["AnalyzerConfig", "VulnerabilityAnalyzer", "analyze"]
